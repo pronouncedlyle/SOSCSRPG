e21b1773a514f017da7ada3546d98dece15c5ea5
Matt <pronouncedlyle@gmail.com>
2021-07-06 17:34:15 -0400

    Created player class attributes and displayed player info in UI

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Matt";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+
+        }
+    }
+}
