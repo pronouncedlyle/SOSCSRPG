2c31444be5656d34d58eda8e617d9ef7f117acb3
Matt <pronouncedlyle@gmail.com>
2021-09-06 14:30:06 -0400

    Added player inventory

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace Engine.Models
 {
@@ -66,6 +67,14 @@ namespace Engine.Models
                 _gold = value;
                 OnPropertyChanged(nameof(Gold));
             }
-        }    
+        }  
+        
+        //Creates class for player inventory
+        public ObservableCollection<GameItem> Inventory { get; set; }
+
+        public Player()
+        {
+            Inventory = new ObservableCollection<GameItem>();
+        }
     }
 }
